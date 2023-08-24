d46af7ca1d702425de256294f639df96f32f5944
andreeraffa <andreeanico18@yahoo.com>
2023-08-24 21:39:24 +0300

    app4

---
+++ modul2_1/app3.py
@@ -0,0 +1,6 @@
+Numar1 = input('Enter Numar1 here: ')
+Numar2 = input('Enter Numar2 here: ')
+
+Media = print('Media numerelor este:',(int(Numar1) + int(Numar2))/2)
+Impartire = print('Impartirea numerelor este: ', int(Numar1)/int(Numar2))
+Ridicare_la_putere = print('A la puterea b: ', int(Numar1) ** int(Numar2))
---
+++ modul2_1/part1.py
@@ -0,0 +1,78 @@
+name = 'Emanuel' # string
+print(name)
+
+age = 37 # int
+print(age)
+
+age = 37.0 # float
+print(age)
+
+age = 37 + 0j # complex
+print(age)
+
+
+result = type(age)
+print(result)
+result = type(age)
+print(result)
+
+print('Methods of string: ', dir(name))
+
+# name.
+# age.
+
+
+
+number1 = 5
+number2 = number1
+number2 = number1 + 1
+#print(number2)
+#print(number1)
+
+#print('Return from print: ', print(37))
+#result1 = input('Input data1 here: ')
+#print(result1)
+#result2 = input('Input data2 here: ')
+#print(result2)
+#print(type(result))
+#print('Result of add: ', result1 + result2)
+#print('Result of add int: ', int(result1) + int(result2))
+#True = 3
+#print(True)
+#print(False)
+#print(type(True))
+#inaltime = input('Enter height: ')
+#baza = input('Enter base: ')
+#result = (int(inaltime) * int(baza)) / 2
+#print(type(result))
+#print('Area of the traingle is: ', result)
+
+#password1 = 7710
+#password = input('Enter password1: ')
+#print(password1 == int(password))
+
+a = 1#0000000
+b = 3#0000000
+print('Memory location a: ', id(a))
+print('Memory location b: ', id(b))
+c = a + b
+print('Memory location c: ', id(c))
+d = 4#0000000
+print('Memory location d: ', id(d))
+
+print(c == d)
+print(c is d) #use for boolean objects
+
+print(id(True))
+print(id(False))
+print(id(True is True))
+
+
+
+
+
+
+
+
+
+
