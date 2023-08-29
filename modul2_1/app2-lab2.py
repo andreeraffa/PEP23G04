c4aa1cff49109f4fd3b6d774d76ca36b4c740414
andreeraffa <andreeanico18@yahoo.com>
2023-08-29 20:06:45 +0300

    part2.py

---
+++ modul2_1/app2-lab2.py
@@ -0,0 +1,10 @@
+Nume = input('Cum te numesti ?: ')
+Varsta = input('Ce varsta ai ?: ')
+
+An_curent =2023
+
+print('Ceau ' ,Nume, '! Deci te-ai nascut in:' ,int(An_curent) - int(Varsta), sep='')
+
+#sau
+
+print(f'Ceau {name}! Deci te-ai nascut in: {2023 - int(varsta)}')
